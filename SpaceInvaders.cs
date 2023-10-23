a868cd1599583c8bdd4f130924960f2f78b8ae7a
Fatih BOZLAK <bozlak.fatih@gmail.com>
2023-10-23 12:28:08 +0200

    Question 2 : Créer la classe SpaceInvaders

---
+++ SpaceInvaders.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bozlak_Fatih_Tp1
+{
+    public class SpaceInvaders
+    {
+        private readonly List<Player> _players = new List<Player>();
+
+        public SpaceInvaders()
+        {
+            this.Init();
+        }
+
+        public List<Player> Players
+        {
+            get => _players;
+        }
+
+        private void AddPlayerToPlayers(Player player)
+        {
+            this._players.Add(player);
+        }
+
+        private void Init()
+        {
+            this.AddPlayerToPlayers(new Player("fatih", "bozlak", "bobylatruffe"));
+            this.AddPlayerToPlayers(new Player("dupond", "deMarchand", "duPondLeMeilleur"));
+            this.AddPlayerToPlayers(new Player("cristiano", "ronaldo", "cr7"));
+        }
+
+        public static void Main(string[] args)
+        {
+            SpaceInvaders spaceInvaders = new SpaceInvaders();
+
+            foreach (Player player in spaceInvaders.Players)
+            {
+                Console.WriteLine(player);
+            }
+        }
+    }
+}
